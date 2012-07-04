b7c62e8d78b78573e4ee495a3dc2ddb51c00f21f
alejandro david weil <tenuki@endlesstruction.com.ar>
2012-07-04 02:33:41 -0300

    bootstrap returns with all its power! and initial-data.yml has a minimal user configuration.

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,19 +1,18 @@
 import play.jobs.*;
+import org.junit.*;
+import play.test.*;
+import models.*;
+ 
  
 @OnApplicationStart
 public class Bootstrap extends Job {
  
     public void doJob() {
         // Check if the database is empty
-        System.out.println("Hello, World");
-        /*if(User.count() == 0) {
-            System.out.println("counter is 0.");
+        if(LgUser.count() == 0) {
+            System.out.println("loading initial-data.yml as no users were found!");
             Fixtures.loadModels("initial-data.yml");
-        };*/ // else {
-           // System.out.println("loading anyways..");
-           // Fixtures.loadModels("initial-data.yml");
-           // }
+        };
     }
- 
 }
 
