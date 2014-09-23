68a0f9516123c5799a21cb5355a09be0002d7496
adji <devnull@localhost>
2014-09-23 08:53:34 -0300

    Archive password.

--- archive.sh
+++ archive.sh
@@ -1,7 +1,8 @@
 LAST_TAG=`hg parents --template '{latesttag}'`
 
+PASS=`cat archive.pwd`
 hg archive "/tmp/cajero_$LAST_TAG.tgz"
 pushd /tmp
-zip -e "cajero_$LAST_TAG.zip" "cajero_$LAST_TAG.tgz"
+zip -e "cajero_$LAST_TAG.zip" "cajero_$LAST_TAG.tgz" -P $PASS
 rm "/tmp/cajero_$LAST_TAG.tgz"
 popd
