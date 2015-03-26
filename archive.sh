72b2857cd01f284e7480a53b820245009c7962ae
Andres Adjimann <aadjiman@gmail.com>
2015-03-26 10:19:03 -0300

    Archive with prefix and a bug fix.

--- archive.sh
+++ archive.sh
@@ -1,6 +1,6 @@
 LAST_TAG=`git describe --abbrev=0 --tags`
 
-git archive --format tgz --output "/tmp/cajero_$LAST_TAGY.tgz" master
+git archive --format tgz --prefix "cajero_$LAST_TAG/" --output "/tmp/cajero_$LAST_TAG.tgz" master
 #hg archive "/tmp/cajero_$LAST_TAGY.tgz"
 pushd /tmp
 zip -e "cajero_$LAST_TAG.zip" "cajero_$LAST_TAG.tgz"
